8b64317d8122d8d84b2dbf12073665bc170dce68
GhostKnight <chaosuncut@gmail.com>
2014-04-26 23:00:59 -0400

    Add in icon pieces.

--- Stratego/src/com/gmu/stratego/board/BoardTile.java
+++ Stratego/src/com/gmu/stratego/board/BoardTile.java
@@ -1,12 +1,16 @@
 package com.gmu.stratego.board;
 
+import com.gmu.stratego.R;
 import com.gmu.stratego.StrategoBoardActivity;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -14,6 +18,7 @@ import android.widget.Toast;
 
 public class BoardTile extends View {
 	final private Paint paint = new Paint();
+	private Drawable currentUnit;
 	private Canvas canvas;
 
 	public BoardTile(Context context, AttributeSet attrs, int defStyleAttr) {
@@ -51,6 +56,10 @@ public class BoardTile extends View {
 	}
 
 	private void draw() {
+		if (currentUnit != null) {
+			
+		}
+		
 		canvas.drawRect(0, 0, 60, 60, paint);
 	}
 	
@@ -62,5 +71,57 @@ public class BoardTile extends View {
 	public void changeBlue() {
 		paint.setColor(Color.BLUE);
 		invalidate();
+	}
+	
+	public void setImage(int teamColor, int unitPower) {
+		int pieceNum = 0;
+		boolean isRed = teamColor == Color.RED;
+		switch(unitPower) {
+		case 1:
+			pieceNum = (isRed) ? R.drawable.r1 : R.drawable.b1;
+			break;
+		case 2:
+			pieceNum = (isRed) ? R.drawable.r2 : R.drawable.b2;
+			break;
+		case 3:
+			pieceNum = (isRed) ? R.drawable.r3 : R.drawable.b3;
+			break;
+		case 4:
+			pieceNum = (isRed) ? R.drawable.r4 : R.drawable.b4;
+			break;
+		case 5:
+			pieceNum = (isRed) ? R.drawable.r5 : R.drawable.b5;
+			break;
+		case 6:
+			pieceNum = (isRed) ? R.drawable.r6 : R.drawable.b6;
+			break;
+		case 7:
+			pieceNum = (isRed) ? R.drawable.r7 : R.drawable.b7;
+			break;
+		case 8:
+			pieceNum = (isRed) ? R.drawable.r8 : R.drawable.b8;
+			break;
+		case 9:
+			pieceNum = (isRed) ? R.drawable.r9 : R.drawable.b9;
+			break;
+		case 10:
+			pieceNum = (isRed) ? R.drawable.r10 : R.drawable.b10;
+			break;
+		case 11:
+			pieceNum = (isRed) ? R.drawable.r11 : R.drawable.b11;
+			break;
+		case 12:
+			pieceNum = (isRed) ? R.drawable.r12 : R.drawable.b12;
+			break;
+		case 13:
+			pieceNum = (isRed) ? R.drawable.r13 : R.drawable.b13;
+			break;
+		}
+		
+		currentUnit = getResources().getDrawable(pieceNum);
+		Rect rect = new Rect(); // TODO: figure out how to do this rectangle bullshit
+		currentUnit.setBounds(rect);
+//		drawable.draw(canvas);
+		
 	}
 }
